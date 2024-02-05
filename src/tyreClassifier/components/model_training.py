ea90a6db25080756cb8fd5ba3926d8a3d13fa25b
Dhananjay Kumar <139283219+dhananjaykmr1991@users.noreply.github.com>
2024-02-05 19:47:07 +0530

    Update model_training.py

--- src/tyreClassifier/components/model_training.py
+++ src/tyreClassifier/components/model_training.py
@@ -1,3 +1,6 @@
+from tyreClassifier.utils.common import *
+from tyreClassifier.logging import logger
+import matplotlib.pyplot as plt
 import os
 from pathlib import Path
 from keras.models import Model
@@ -11,6 +14,8 @@ from sklearn.model_selection import KFold
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import GridSearchCV,RandomizedSearchCV
 import pickle 
+from keras import backend as K
+K.clear_session()
 
 @dataclass(frozen=True)
 class ModelTrainingConfig:
@@ -28,60 +33,74 @@ class ModelSelction:
         self.config=config
 
     def model_VGG16(self):
+        try:
+            vgg = VGG16(input_shape = self.config.IMAGE_SIZE + [3], weights = 'imagenet', include_top = False)
+           
+            for layer in vgg.layers:
+                layer.trainable = False
 
-        vgg = VGG16(input_shape = self.config.IMAGE_SIZE + [3], weights = 'imagenet', include_top = False)
+            x = Flatten()(vgg.output)
+            x = Dense(self.config.num_classes, activation = 'softmax')(x)
+            model = Model(inputs = vgg.input, outputs = x)
+            model.compile(optimizer ='adam',loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+            logger.info("Building CNN model using 'VGG16'")
+
+            return model
         
-        for layer in vgg.layers:
-            layer.trainable = False
-
-        x = Flatten()(vgg.output)
-        x = Dense(self.config.num_classes, activation = 'softmax')(x)
-        model = Model(inputs = vgg.input, outputs = x)
-        model.compile(optimizer ='adam',loss='sparse_categorical_crossentropy', metrics=['accuracy'])
-
-        return model
+        except Exception as e:
+            raise e
     
     
     def model_resnet_50(self):
+        try:
+            resnet_50 = ResNet50(input_shape = self.config.IMAGE_SIZE + [3], weights = 'imagenet', include_top = False)
+            
+            for layer in resnet_50.layers:          
+                layer.trainable = False
 
-        resnet_50 = ResNet50(input_shape = self.config.IMAGE_SIZE + [3], weights = 'imagenet', include_top = False)
-        
-        for layer in resnet_50.layers:          
-            layer.trainable = False
+            x = Flatten()(resnet_50.output)
+            '''x = Dense(512, activation='relu')(x) 
+    
+            x = Dense(256, activation='relu')(x) 
+    
+            x = Dense(128, activation='relu')(x) 
+    
+            x = Dense(64, activation='relu')(x) 
+            #x = Dropout(0.5)(x)'''
+            predictions = Dense(self.config.num_classes, activation='softmax')(x)
+            model = Model(inputs = resnet_50.input, outputs = predictions)
+            model.compile(optimizer="adam", loss="sparse_categorical_crossentropy", metrics=["accuracy"])
+            logger.info("Building CNN model using 'ResNet-50'")
 
-        num_classes=2
-        x = resnet_50.output
-        x = GlobalAveragePooling2D()(x)
-        x = Dense(512, activation='relu')(x) 
-        x = Dropout(0.5)(x)
-        x = Dense(256, activation='relu')(x) 
-        x = Dropout(0.5)(x)
-        x = Dense(128, activation='relu')(x) 
-        x = Dropout(0.5)(x)
-        x = Dense(64, activation='relu')(x) 
-        x = Dropout(0.5)(x)
-        predictions = Dense(self.config.num_classes, activation='softmax')(x)
-        model = Model(inputs = resnet_50.input, outputs = predictions)
-        model.compile(optimizer="adam", loss="sparse_categorical_crossentropy", metrics=["accuracy"])
-
-        return model
+            return model
+        except Exception as e:
+            raise e
     
     def model_KNN(self):
-        model = KNeighborsClassifier()
+        try:
+            model = KNeighborsClassifier()
 
-        kf=KFold(n_splits=10,shuffle=True,random_state=42)
-        parameter={'n_neighbors': np.arange(30, 55, 2)}
-        model=RandomizedSearchCV(estimator = model, param_distributions=parameter, cv=kf, verbose=1)
-        return model
+            kf=KFold(n_splits=5,shuffle=True,random_state=42)
+            parameter={'n_neighbors': np.arange(30, 55, 2)}
+            model=RandomizedSearchCV(estimator = model, param_distributions=parameter, cv=kf, verbose=2)
+            logger.info("Building machine learning model using 'KNN Classifier'")
+            return model
+        except Exception as e:
+            raise e
     
     def model_RandomForest(self):
-        model = RandomForestClassifier(n_estimators=150) 
-        param_dist = {"max_depth": [3,40], "max_features": [3, 50], "min_samples_split": [2, 15], 
-                      "bootstrap": [True, False], "criterion": ["gini", "entropy"]} 
-        model = RandomizedSearchCV(estimator = model, param_distributions = param_dist, n_iter = 100, 
-                                   cv = 3, verbose=2, random_state=42, n_jobs = -1)
-        
-        return model
+        try:
+            model = RandomForestClassifier(n_estimators=150) 
+            param_dist = {"max_depth": [3,40], "max_features": [3, 50], "min_samples_split": [10, 20], 
+                        "criterion": ["gini", "entropy"]} 
+            model = RandomizedSearchCV(estimator = model, param_distributions = param_dist, n_iter = 4, 
+                                    cv = 3, verbose=2, random_state=42, n_jobs = -1)
+            
+            logger.info("Building machine learning model using 'Random Forest'")
+            
+            return model
+        except Exception as e:
+            raise e
 
 
 
@@ -90,38 +109,61 @@ class ModelTraining:
         self.config=config
 
     def KNN_training(self,model):
-        model.fit(self.config.flat_train_images,self.config.flat_train_labels)
-        saved_model = pickle.dumps(model)  
-        pickle.dump(model, open(Path('models\KNN.pkl'), 'wb'))
-
-        return model
+        try:
+            model.fit(self.config.flat_train_images,self.config.flat_train_labels)  
+            pickle.dump(model, open(Path('models\KNN.pkl'), 'wb'))
+            logger.info("raining machine learning model using 'KNN Classifier' and saving at 'models\KNN.pkl'")
+            return model
+        
+        except Exception as e:
+            raise e
     
     def RandomForest_training(self,model):
-        model.fit(self.config.flat_train_images,self.config.flat_train_labels)
-        saved_model = pickle.dumps(model)  
-        pickle.dump(model, open(Path('models\RandomForest.pkl'), 'wb'))
+        try:
+            model.fit(self.config.flat_train_images,self.config.flat_train_labels) 
+            pickle.dump(model, open(Path('models\RandomForest.pkl'), 'wb'))
+            logger.info("raining machine learning model using 'Random Forest' and saving at 'models\RandomForest.pkl'")
 
-        return model
+            return model
+        except Exception as e:
+            raise e
 
 
 
-    def vgg16_training(self,model):
-        checkpoint =ModelCheckpoint("models\vgg16.h5", save_best_only=True,save_weights_only=False)
-        early_stopping =EarlyStopping(patience=5, restore_best_weights=True)
-        learning_rate_reduction = ReduceLROnPlateau(monitor = 'val_accuracy',patience = 2,verbose = 1,factor = 0.3, min_lr = 0.000001)
-        model=model.fit(self.config.train_images,self.config.train_labels,
-                                  epochs=10,validation_data=(self.config.val_images, self.config.val_labels),
-                                 callbacks=[checkpoint,early_stopping,ReduceLROnPlateau]) 
-     
-        return model
+    def vgg16_training(self, model):
+        try:
+            epochs = 10
+            checkpoint = ModelCheckpoint("models/vgg16.h5", save_best_only=True, save_weights_only=False)
+            early_stopping = EarlyStopping(patience=5, restore_best_weights=True)
+            learning_rate_reduction = ReduceLROnPlateau(monitor='val_accuracy', patience=2, verbose=1, factor=0.3, min_lr=0.000001)
+            history = model.fit(self.config.train_images, self.config.train_labels,
+                                batch_size=32, epochs=epochs, validation_data=(self.config.val_images, self.config.val_labels,),
+                                callbacks=[checkpoint, early_stopping, learning_rate_reduction])
+            plot_accuracy_loss_chart(history,'VGG16',epochs)
+            logger.info("Training machine learning model using 'VGG16' and saving at 'models/vgg16.h5'")
+
+            return history
+        except Exception as e:
+            raise e
+
     
     def resnet_50_training(self,model):
-        checkpoint =ModelCheckpoint("models\resnet_50.h5", save_best_only=True,save_weights_only=False)
-        early_stopping =EarlyStopping(patience=5, restore_best_weights=True)
-        learning_rate_reduction = ReduceLROnPlateau(monitor = 'val_accuracy',patience = 2,verbose = 1,factor = 0.3, min_lr = 0.000001)
-        model.fit(self.config.train_images,self.config.train_labels,epochs=10,
-                  validation_data=(self.config.val_images, self.config.val_labels),
-                  callbacks=[checkpoint,early_stopping,ReduceLROnPlateau])
-        
-        return model
+        try:
+            epochs = 15
+            checkpoint =ModelCheckpoint("models/resnet_50.h5", save_best_only=True,save_weights_only=False)
+            early_stopping =EarlyStopping(patience=5, restore_best_weights=True)
+            learning_rate_reduction = ReduceLROnPlateau(monitor='val_accuracy', patience=2, verbose=1, factor=0.2, min_lr=0.000001)
+            history = model.fit(self.config.train_images, self.config.train_labels,
+                                batch_size=32, epochs=epochs, validation_data=(self.config.val_images, self.config.val_labels,),
+                                callbacks=[checkpoint, early_stopping, learning_rate_reduction])
+            plot_accuracy_loss_chart(history,'ResNet_50',epochs)
+            logger.info("Training machine learning model using 'ResNet-50' and saving at 'models/resnet_50.h5'")
+            return history
+        except Exception as e:
+            raise e
+    
+
+
+
+
     
